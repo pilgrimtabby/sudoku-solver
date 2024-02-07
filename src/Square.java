c9abca678441cfde340981378e801bc876960a77
pilgrim_tabby <pilgrimtabby@gmail.com>
2024-02-08 00:36:32 +0900

    Update Square.java
    Trying to make this more efficient by removing superfluous square checks

--- src/Square.java
+++ src/Square.java
@@ -1,197 +1,353 @@
-import java.util.ArrayList;
-
 /**
- * Hold information about an individual square on a sudoku board.
+ * Hold the contents of a sudoku board.
  *
  * @author pilgrim_tabby
- * @version 0.0.1
+ * @version 0.0.2
  */
-public class Square implements Comparable<Square> {
-    private int value;
-    /** List of possible values / solutions. */
-    private ArrayList<Integer> possible;
-    private final int row;
-    private final int col;
+public class SudokuBoard implements Comparable<SudokuBoard> {
+    private final Square[][] grid = new Square[9][9];
+    /** Number of squares with non-zero (solved) value */
+    private int filled;
+    private int squaresChecked;
+    /** Squares with 1 possible solution */
+    private final Stack<Square> solvedSquares = new Stack<>();
 
     /**
-     * Default square constructor. Used to create an intentionally
-     * bad default for this.priority (it will always be replaced
-     * when running SudokuBoard.update).
+     * Construct new board from 9 x 9 integer nested array.
+     * @param grid The nested integer array of (1-digit) numbers.
      */
-    public Square() {
-        this.value = 0;
-        // Create possible solutions list with 10 entries.
-        this.possible = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            this.possible.add(i);
-        }
-        this.row = 0;
-        this.col = 0;
-    }
-
-    /**
-     * Construct a square from its value and location on a new board.
-     * @param grid The grid being used to construct a new board.
-     * @param row 0 to 8
-     * @param col 0 to 8
-     */
-    public Square(int[][] grid, int row, int col) {
-        this.value = grid[row][col];
-        this.row = row;
-        this.col = col;
-        setPossible(grid);
-    }
-
-    /**
-     * Construct a square from another square.
-     * @param square The old square to copy.
-     */
-    public Square(Square square) {
-        this.value = square.value;
-        // Make deep copy of possible solutions
-        if (this.value == 0) {
-            this.possible = new ArrayList<>();
-            this.possible.addAll(square.possible);
-        }
-        this.row = square.row;
-        this.col = square.col;
-    }
-    
-    /**
-     * Setter method for this.value.
-     * @param i The square's value.
-     */
-    public void setValue(int i) {
-        this.value = i;
-    }
-
-    /**
-     * Getter method for this.value.
-     * @return The value.
-     */
-    public int getValue() {
-        return this.value;
-    }
-
-    /**
-     * Force a square to have only 1 possibility.
-     * @param i The possible solution.
-     */
-    public void overwritePossible(int i) {
-        this.possible.clear();
-        this.possible.add(i);
-    }
-
-    /**
-     * Getter method for this.possible.
-     * @return ArrayList of possible solutions.
-     */
-    public ArrayList<Integer> getPossible() {
-        return this.possible;
-    }
-
-    /**
-     * Delete one possible solution from this.possible.
-     * @param i The possible solution to delete (not an index!).
-     */
-    public void delPossible(int i) {
-        this.possible.remove(Integer.valueOf(i));
-    }
-
-    /**
-     * Getter method for this.row.
-     * @return the square's row.
-     */
-    public int getRow() {
-        return row;
-    }
-
-    /**
-     * Getter method for this.col.
-     * @return the square's column.
-     */
-    public int getCol() {
-        return col;
-    }
-
-    /**
-     * Setter method for this.possible when first board is created.
-     * @param grid The grid of values used to create a new board.
-     */
-    private void setPossible(int[][] grid) {
-        this.possible = new ArrayList<>();
-        ArrayList<Integer> existing = new ArrayList<>(8);
-        existing.addAll(getRowValues(grid));
-        existing.addAll(getColValues(grid));
-        existing.addAll(getBoxValues(grid));
-
-        // Add possible solutions not in surrounding cells
-        for (int i = 1; i <= 9; i++) {
-            if (!existing.contains(i)) {
-                this.possible.add(i);
-            }
-        }
-    }
-
-    /**
-     * Get values of all solved squares in a row.
-     * @param grid The 9 x 9 number grid used to make a new board.
-     * @return ArrayList of solved square values in a row.
-     */
-    private ArrayList<Integer> getRowValues(int[][] grid) {
-        ArrayList<Integer> existing = new ArrayList<>();
-        for (int i = 0; i <= 8; i++) {
-            int val = grid[this.row][i];
-            if (val != 0) {
-                existing.add(val);
-            }
-        }
-        return existing;
-    }
-
-    /**
-     * Get values of all solved squares in a column.
-     * @param grid The 9 x 9 number grid used to make a new board.
-     * @return ArrayList of solved square values in a column.
-     */
-    private ArrayList<Integer> getColValues(int[][] grid) {
-        ArrayList<Integer> existing = new ArrayList<>();
-        for (int i = 0; i <= 8; i++) {
-            int val = grid[i][this.col];
-            if (val != 0) {
-                existing.add(val);
-            }
-        }
-        return existing;
-    }
-
-    /**
-     * Get values of all solved squares in a 3 x 3 box.
-     * @param grid The 9 x 9 number grid used to make a new board.
-     * @return ArrayList of solved square values in a 3 x 3 box.
-     */
-    private ArrayList<Integer> getBoxValues(int[][] grid) {
-        ArrayList<Integer> existing = new ArrayList<>();
-        int boxTopRow = (this.row / 3) * 3;
-        int boxLeftCol = (this.col / 3) * 3;
-        for (int i = boxTopRow; i <= boxTopRow + 2; i++) {
-            for (int j = boxLeftCol; j <= boxLeftCol + 2; j++) {
-                int val = grid[i][j];
-                if (val != 0) {
-                    existing.add(val);
+    public SudokuBoard(int[][] grid) {
+        // Fill this.grid with Square objects
+        for (int row = 0; row <= 8; row++) {
+            for (int col = 0; col <= 8; col++) {
+                this.grid[row][col] = new Square(grid, row, col);
+                if (this.grid[row][col].getPossible().size() == 1) {
+                    this.solvedSquares.push(this.grid[row][col]);
                 }
             }
         }
-        return existing;
+
+        // Set this.filled to count of current solved squares
+        setFilled();
+        this.squaresChecked = 0;
+    }
+
+    /**
+     * Construct new board from old board by deep copying its squares
+     * @param board The old board
+     */
+    public SudokuBoard(SudokuBoard board) {
+        // Deep copy squares of old board to new board
+        for (Square[] row : board.grid) {
+            for (Square square : row) {
+                this.grid[square.getRow()][square.getCol()] = new Square(square);
+                if (this.grid[square.getRow()][square.getCol()].getPossible() != null && this.grid[square.getRow()][square.getCol()].getPossible().size() == 1) {
+                    this.solvedSquares.push(this.grid[square.getRow()][square.getCol()]);
+                }
+            }
+        }
+
+        this.filled = board.filled;
+        this.squaresChecked = board.squaresChecked;
+    }
+
+    /**
+     * Iterate over each square and update its possible.
+     * Runs in a loop until no square's value is updated.
+     * @return false if any square is unsolvable, true otherwise.
+     */
+    public Square updateGrid() {
+        while (!this.solvedSquares.isEmpty()) {
+            boolean boardIsPossible = update(this.solvedSquares.pop());
+            this.squaresChecked++;
+            if (!boardIsPossible) {
+                System.out.println("impossible board");
+                return null;
+            }
+        }
+        System.out.println("stack empty");
+        return getPriority();
+    }
+
+    /**
+     * Verify a board is obeying "sudoku rules" (no duplicates of
+     * 1-9 in a column, row, or 3x3 box).
+     * @return false if a rule is broken, true otherwise.
+     */
+    public boolean validBoard() {
+        return validInput() && validRows() && validColumns() && validBoxes();
+    }
+
+    /**
+     * Getter method for this.priority.
+     * @return square with the lowest number of possible solutions.
+     */
+    public Square getPriority() {
+        if (getFilled() == 81) {
+            return null;
+        }
+
+        Square priority = null;
+        for (Square[] row : this.grid) {
+            for (Square square : row) {
+                if (square.getValue() == 0) {
+                    priority = square;
+                    break;
+                }
+            }
+        }
+
+        for (Square[] row : this.grid) {
+            for (Square square : row) {
+                if (square.getPossible() != null && square.compareTo(priority) < 0) {
+                    priority = square;
+                    if (priority.getPossible().size() == 2) {
+                        return priority;
+                    }
+                }
+            }
+        }
+        return priority;
+    }
+
+    /**
+     * Getter method for this.filled.
+     * @return number of cells with non-zero values.
+     */
+    public int getFilled() {
+        return this.filled;
+    }
+
+    /**
+     * Set this.filled to the number of squares with non-zero values.
+     */
+    private void setFilled() {
+        this.filled = 0;
+        for (Square[] row : this.grid) {
+            for (Square square : row) {
+                if (square.getValue() != 0) {
+                    this.filled++;
+                }
+            }
+        }
+    }
+
+    /**
+     * Verify each value in the board is a single-digit integer.
+     * @return true if only single-digit integers, false otherwise.
+     */
+    private boolean validInput() {
+        for (int row = 0; row <= 8; row++) {
+            for (int col = 0; col <= 8; col++) {
+                if (this.grid[row][col].getValue() < 0 || this.grid[row][col].getValue() > 9) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Verify each row contains no duplicate values.
+     * @return true if each value is unique, false otherwise.
+     */
+    private boolean validRows() {
+        for (int row = 0; row <= 8; row++) {
+            boolean[] contents = {false, false, false, false, false, false, false, false, false};
+            for (int col = 0; col <= 8; col++) {
+                // Ignore "blank" squares
+                if (this.grid[row][col].getValue() == 0) {
+                    continue;
+                }
+                // Digit is a duplicate = invalid
+                if (contents[this.grid[row][col].getValue() - 1]) {
+                    return false;
+                }
+                contents[this.grid[row][col].getValue() - 1] = true;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Verify each column contains no duplicate values.
+     * @return true if each value is unique, false otherwise.
+     */
+    private boolean validColumns() {
+        for (int col = 0; col <= 8; col++) {
+            boolean[] contents = {false, false, false, false, false, false, false, false, false};
+            for (int row = 0; row <= 8; row++) {
+                if (this.grid[row][col].getValue() == 0) {
+                    continue;
+                }
+                if (contents[this.grid[row][col].getValue() - 1]) {
+                    return false;
+                }
+                contents[this.grid[row][col].getValue() - 1] = true;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Verify each 3 x 3 box contains no duplicate values.
+     * @return true if each value is unique, false otherwise.
+     */
+    private boolean validBoxes() {
+        for (int topRow = 0; topRow <= 6; topRow+=3) {
+            for (int leftCol = 0; leftCol <= 6; leftCol+=3) {
+
+                boolean[] contents = {false, false, false, false, false, false, false, false, false};
+                for (int row = topRow; row <= topRow + 2; row++) {
+                    for (int col = leftCol; col <= leftCol + 2; col++) {
+
+                        if (this.grid[row][col].getValue() == 0) {
+                            continue;
+                        }
+                        if (contents[this.grid[row][col].getValue() - 1]) {
+                            return false;
+                        }
+                        contents[this.grid[row][col].getValue() - 1] = true;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Updates possible solutions for each square.
+     * If a square is already solved, return true.
+     * If a square has no possible solutions, return false (invalid).
+     * If a square has 1 possible solution, set the square's value,
+     * and update the surrounding squares' possible solutions.
+     * If a square has multiple possible solutions, set this.priority
+     * to the square with fewer possible solutions.
+     * @param square The square.
+     * @return false if a square has no possible solution, else true.
+     */
+    private boolean update(Square square) {
+        if (square.getValue() != 0) { return true; }
+        if (square.getPossible().isEmpty()) { return false; }
+
+        if (square.getPossible().size() == 1) {
+            int value = square.getPossible().get(0);
+            square.setValue(value);
+            this.filled++;
+            return updateNeighbors(square);
+        }
+        return true;
+    }
+
+    /**
+     * After setting a square's value, check all squares in the same
+     * row / column / 3x3 grid and update their possible values. If
+     * this leaves a square with no possible solutions, return false.
+     * @param square The square with the recently set value, whose
+     *               neighbors are being checked.
+     * @return false if a square becomes impossible, true otherwise.
+     */
+    private boolean updateNeighbors(Square square) {
+        return updateRow(square) && updateCol(square) && updateBox(square);
+    }
+
+    /**
+     * After setting a square's value, change possible solutions for
+     * squares in the same row.
+     * @param square The square whose value was changed.
+     * @return false if any square becomes unsolvable, else true.
+     */
+    private boolean updateRow(Square square) {
+        int formerPossibleCount;
+        for (int col = 0; col <= 8; col++) {
+            Square neighbor = this.grid[square.getRow()][col];
+            if (neighbor.getValue() == 0) {
+                formerPossibleCount = neighbor.getPossible().size();
+                neighbor.delPossible(square.getValue());
+                if (neighbor.getPossible().isEmpty()) {
+                    return false;
+                } else if (neighbor.getPossible().size() == 1 && formerPossibleCount != 1) {
+                    this.solvedSquares.push(neighbor);
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * After setting a square's value, change possible solutions for
+     * squares in the same column.
+     * @param square The square whose value was changed.
+     * @return false if any square becomes unsolvable, else true.
+     */
+    private boolean updateCol(Square square) {
+        int formerPossibleCount;
+        for (int row = 0; row <= 8; row++) {
+            Square neighbor = this.grid[row][square.getCol()];
+            if (neighbor.getValue() == 0) {
+                formerPossibleCount = neighbor.getPossible().size();
+                neighbor.delPossible(square.getValue());
+                if (neighbor.getPossible().isEmpty()) {
+                    return false;
+                } else if (neighbor.getPossible().size() == 1 && formerPossibleCount != 1) {
+                    this.solvedSquares.push(neighbor);
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * After setting a square's value, change possible solutions for
+     * squares in the same 3x3 box.
+     * @param square The square whose value was changed.
+     * @return false if any square becomes unsolvable, else true.
+     */
+    private boolean updateBox(Square square) {
+        int formerPossibleCount;
+        int boxTopRow = (square.getRow() / 3) * 3;
+        int boxLeftCol = (square.getCol() / 3) * 3;
+        for (int row = boxTopRow; row <= boxTopRow + 2; row++) {
+            for (int col = boxLeftCol; col <= boxLeftCol + 2; col++) {
+                Square neighbor = this.grid[row][col];
+                if (neighbor.getValue() == 0) {
+                    formerPossibleCount = neighbor.getPossible().size();
+                    neighbor.delPossible(square.getValue());
+                    if (neighbor.getPossible().isEmpty()) {
+                        return false;
+                    } else if (neighbor.getPossible().size() == 1 && formerPossibleCount != 1) {
+                        this.solvedSquares.push(neighbor);
+                    }
+                }
+            }
+        }
+        return true;
     }
 
     @Override
-    public int compareTo(Square o) {
-        return Integer.compare(this.possible.size(), o.possible.size());
+    public int compareTo(SudokuBoard o) {
+        return Integer.compare(this.filled, o.filled);
     }
 
     @Override
     public String toString() {
-        return String.valueOf(this.value);
+        StringBuilder sb = new StringBuilder();
+        for (int row = 0; row <= 8; row++) {
+            for (int column = 0; column <= 8; column++) {
+                sb.append(this.grid[row][column]);
+                sb.append("  ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
+
+    /**
+     * Getter method for this.squaresChecked.
+     * @return total number of squares iterated over in update method.
+     */
+    public int getSquaresChecked() {
+        return this.squaresChecked;
+    }
+
 }
